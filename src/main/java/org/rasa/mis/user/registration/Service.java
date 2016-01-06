e29d4b43e5ef5efe37c917a2eea861ab0602fc02
Rasanjaya Subasinghe <rasanjaya@wso2.com>
2016-01-07 00:50:28 +0530

    Refactor service class

--- src/main/java/org/rasa/mis/user/registration/Service.java
+++ src/main/java/org/rasa/mis/user/registration/Service.java
@@ -1,7 +1,5 @@
 package org.rasa.mis.user.registration;
 
-import org.rasa.mis.user.registration.beans.User;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -12,22 +10,14 @@ import javax.ws.rs.core.Response;
  * User Registration Service
  */
 @Path("/")
-public class Service {
-
+public interface Service {
     @GET
     @Produces("text/plain")
     @Path("/{param}")
-    public Response test(@PathParam("param") String parameter) {
-        String output = "Roger that \"" + parameter + "\"";
-        return Response.status(200).entity(output).build();
-    }
+    public Response test(@PathParam("param") String parameter);
 
     @GET
     @Produces("application/json")
     @Path("/users/{param}")
-    public Response getUser(@PathParam("param") String parameter) {
-        User user = new User();
-        user.setFirstName(parameter);
-        return Response.status(200).entity(user).build();
-    }
+    public Response getUser(@PathParam("param") String parameter);
 }
