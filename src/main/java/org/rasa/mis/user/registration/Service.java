96d87287c3a1679c4ebc3dba21174767de9b68d0
Rasanjaya Subasinghe <rasanjaya@wso2.com>
2016-01-06 21:22:29 +0530

    Refactor service

---
+++ src/main/java/org/rasa/mis/user/registration/Service.java
@@ -0,0 +1,23 @@
+package org.rasa.mis.user.registration;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+/**
+ * User Registration Service
+ */
+@Path("/api")
+public class Service {
+
+    @GET
+    @Produces("text/plain")
+    @Path("/{param}")
+    public Response getClichedMessage(@PathParam("param") String parameter) {
+        String output = "Hello World " + parameter;
+        return Response.status(200).entity(output).build();
+    }
+
+}
